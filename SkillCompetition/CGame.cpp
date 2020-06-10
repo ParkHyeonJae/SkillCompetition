85fc680d92169349e69ac457358dc8aa422d028d
ParkHyeonJae <md101106@gmail.com>
2020-06-10 23:36:07 +0900

    ADD: Any Sources

--- SkillCompetition/CGame.cpp
+++ SkillCompetition/CGame.cpp
@@ -13,10 +13,36 @@ INT CGame::Init()
 {
 	CDX2DAPP::Init();
 
+	srand((unsigned int)time(0));
+
 	m_sprite = new CSprite(L"../Images/rocket.png", m_Gfx);
 	m_PlayerPos = { (float)(m_dScnX / 2),(float)(m_dScnY / 2) };
 	m_rot = 0.0f;
 	m_PlayerScale = { 1.0f,1.0f };
+	m_playerCenter.x = m_PlayerPos.x + (m_sprite->GetSize().width / 2);
+	m_playerCenter.y = m_PlayerPos.y + (m_sprite->GetSize().height / 2);
+	m_rtPlayerBox = { (LONG)m_playerCenter.x - 120, (LONG)m_playerCenter.y - 120, (LONG)m_playerCenter.x + 120, (LONG)m_playerCenter.y + 120 };
+	m_bIsCrash = FALSE;
+	m_bIsCircleCrash = FALSE;
+
+	LONG RandX = (LONG)(rand() % m_dScnX + 1);
+	LONG RandY = (LONG)(rand() % m_dScnY + 1);
+	m_rtTargetBox = { RandX, RandY, RandX + (LONG)(rand() % (m_dScnX - RandX) + 1), RandY + (LONG)(rand() % (m_dScnY - RandY) + 1)};
+
+	LONG RandX1 = (LONG)(rand() % m_dScnX + 1);
+	LONG RandY1 = (LONG)(rand() % m_dScnY + 1);
+	m_rtTargetBox2 = { RandX, RandY, RandX + (LONG)(rand() % (m_dScnX - RandX) + 1), RandY + (LONG)(rand() % (m_dScnY - RandY) + 1) };
+
+
+	m_TargetCircle = { (LONG)(rand() % m_dScnX + 1) , (LONG)(rand() % m_dScnY + 1) };
+	radius = rand() % (m_dScnX - m_dScnY);
+
+
+	m_TargetCircle2 = { (LONG)(rand() % m_dScnX + 1) , (LONG)(rand() % m_dScnY + 1) };
+	radius2 = rand() % (m_dScnX - m_dScnY);
+
+	m_bOnceGeometry[0] = TRUE;
+	m_bOnceGeometry[1] = TRUE;
 	return 0;
 }
 
@@ -25,14 +51,30 @@ INT CGame::Render()
 	m_Gfx->BeginDraw();
 	m_Gfx->ClearScreen(0.0f, 0.0f, 0.5f);
 	m_Gfx->GetRenderTarget()->SetTransform(D2D1::Matrix3x2F::Identity());
-	D2D1_POINT_2F center;
-	center.x = m_PlayerPos.x + (m_sprite->GetSize().width / 2);
-	center.y = m_PlayerPos.y + (m_sprite->GetSize().height / 2);
-	m_sprite->Draw(m_PlayerPos, &center, D2D1::Point2F(m_PlayerScale.x, m_PlayerScale.y), m_rot);
+
+	
+
+	m_sprite->Draw(m_PlayerPos, &m_playerCenter, D2D1::Point2F(m_PlayerScale.x, m_PlayerScale.y), m_rot);
 	m_Gfx->DrawTextOut(L"20106 박현재", D2D1::Point2F(m_PlayerPos.x - 50, m_PlayerPos.y - 30));
 
 	m_Gfx->GetRenderTarget()->SetTransform(D2D1::Matrix3x2F::Identity());
+	//m_Gfx->DrawRectangle((float)m_rtPlayerBox.left, (float)m_rtPlayerBox.top, (float)m_rtPlayerBox.right, (float)m_rtPlayerBox.bottom, 1.0f, 1.0f, 1.0f, 1.0f);
+	m_Gfx->DrawRectangle((float)m_rtTargetBox.left, (float)m_rtTargetBox.top, (float)m_rtTargetBox.right, (float)m_rtTargetBox.bottom, 1.0f, 1.0f, 1.0f, 1.0f);
+	m_Gfx->DrawCircle((float)m_TargetCircle.x, (float)m_TargetCircle.y, radius, 1.0f, 1.0f, 1.0f, 1.0f);
+
+	if (m_bOnceGeometry[0])
+	{
+		m_Gfx->DrawRectangle((float)m_rtTargetBox2.left, (float)m_rtTargetBox2.top, (float)m_rtTargetBox2.right, (float)m_rtTargetBox2.bottom, 1.0f, 1.0f, 1.0f, 1.0f);
+		
+	}
+	if (m_bOnceGeometry[1])
+	{
+		m_Gfx->DrawCircle((float)m_TargetCircle2.x, (float)m_TargetCircle2.y, radius2, 1.0f, 1.0f, 1.0f, 1.0f);
+
+	}
+	
 	m_Gfx->DrawTextOut(L"이동: 방향키\n 회전: 마우스 방향\n 확대/축소: 마우스 좌/우 클릭", D2D1::Point2F());
+
 
 	m_Gfx->EndDraw();
 	return 0;
@@ -40,9 +82,41 @@ INT CGame::Render()
 
 INT CGame::FrameMove(DWORD elpased)
 {
-
 	CDX2DAPP::FrameMove(elpased);
 
+	m_playerCenter.x = m_PlayerPos.x + (m_sprite->GetSize().width / 2);
+	m_playerCenter.y = m_PlayerPos.y + (m_sprite->GetSize().height / 2);
+	m_rtPlayerBox = { (LONG)m_playerCenter.x - 120,
+		(LONG)m_playerCenter.y - 120,
+		(LONG)m_playerCenter.x + 120,
+		(LONG)m_playerCenter.y + 120 };
+	if (IsRectInRect(m_rtTargetBox, m_rtPlayerBox)) {
+		m_bIsCrash = TRUE;
+	}
+	
+	if (m_bIsCrash) {
+		LONG RandX = (LONG)(rand() % m_dScnX);
+		LONG RandY = (LONG)(rand() % m_dScnY);
+		m_rtTargetBox = { RandX, RandY, RandX + (LONG)(rand() % (m_dScnX - RandX) + 1), RandY + (LONG)(rand() % (m_dScnY - RandY) + 1) };
+		m_bIsCrash = FALSE;
+	}
+
+	if (IsRectInCircle(m_TargetCircle, radius, m_rtPlayerBox)){
+		m_bIsCircleCrash = TRUE;
+	}
+	if (m_bIsCircleCrash)
+	{
+		m_TargetCircle = { (LONG)(rand() % m_dScnX + 1) , (LONG)(rand() % m_dScnY + 1) };
+		radius = rand() % (m_dScnX - m_dScnY) + 1;
+		m_bIsCircleCrash = FALSE;
+	}
+
+	if (IsRectInRect(m_rtTargetBox2, m_rtPlayerBox)) {
+		m_bOnceGeometry[0] = FALSE;
+	}
+	if (IsRectInCircle(m_TargetCircle2, radius2, m_rtPlayerBox)) {
+		m_bOnceGeometry[1] = FALSE;
+	}
 	return 0;
 }
 
@@ -57,15 +131,17 @@ INT CGame::Control(CInput* Input)
 	float rotdegree = atan2f(m_PlayerPos.y - MousePos.y, m_PlayerPos.x - MousePos.x);
 	
 	m_rot = (rotdegree * (180 / PI)) - 90;
+
+	
 	//printf("%f\n", m_rot);
 
-	if (Input->KeyPress(VK_LEFT) && m_PlayerPos.x > 0)
+	if (Input->KeyPress(VK_LEFT))
 		m_PlayerPos.x -= 10;
-	if (Input->KeyPress(VK_RIGHT) && m_PlayerPos.x< (m_dScnX - m_sprite->GetSize().width))
+	if (Input->KeyPress(VK_RIGHT))
 		m_PlayerPos.x += 10;
-	if (Input->KeyPress(VK_UP) && m_PlayerPos.y > 0)
+	if (Input->KeyPress(VK_UP))
 		m_PlayerPos.y -= 10;
-	if (Input->KeyPress(VK_DOWN) && m_PlayerPos.y< (m_dScnY - m_sprite->GetSize().height))
+	if (Input->KeyPress(VK_DOWN))
 		m_PlayerPos.y += 10;
 
 
@@ -79,6 +155,9 @@ INT CGame::Control(CInput* Input)
 		m_PlayerScale.x -= 0.1f;
 		m_PlayerScale.y -= 0.1f;
 	}
+
+
+
 	return 0;
 }
 
---
+++ SkillCompetition/Utility.cpp
@@ -0,0 +1,74 @@
+#include "framework.h"
+
+BOOL IsPointInRect(const RECT rt, const POINT pt)
+{
+	if ((rt.left <= pt.x && rt.right >= pt.x) &&
+		(rt.top <= pt.y && rt.bottom >= pt.y))
+	{
+		return TRUE;
+	}
+	return FALSE;
+}
+
+BOOL IsPointInCircle(const POINT ptCircle, const int cr, const POINT pt)
+{
+	float dx = ptCircle.x - pt.x;
+	float dy = ptCircle.y - pt.y;
+	float l = sqrt(dx * dx + dy * dy);
+
+	if (l <= cr) return TRUE;
+	
+	return FALSE;
+	return 0;
+}
+
+RECT* rtIntersectRect(const RECT rt1, const RECT rt2)
+{
+	RECT* pIntersect = new RECT();
+	*pIntersect = RECT{ 0,0,0,0 };
+
+	if (rt1.left < rt2.right && rt1.right > rt2.left)
+	{
+		pIntersect->left = max(rt1.left, rt2.left);
+		pIntersect->right = min(rt1.right, rt2.right);
+	}
+	if (rt1.top < rt2.bottom && rt1.bottom > rt2.top)
+	{
+		pIntersect->top = max(rt1.top, rt2.top);
+		pIntersect->bottom = min(rt1.bottom, rt2.bottom);
+	}
+
+	return pIntersect;
+}
+
+BOOL IsRectInRect(const RECT rt1, const RECT rt2)
+{
+	if (rt1.right >= rt2.left && rt1.bottom >= rt2.top
+		&& rt1.left <= rt2.right && rt1.top <= rt2.bottom) {
+		
+		return TRUE;
+
+	}
+	return FALSE;
+}
+BOOL IsRectInCircle(POINT ptCircle, int radius, const RECT rt)
+{
+	RECT newRt = { rt.left - radius, rt.top - radius, rt.right + radius ,rt.bottom + radius };
+
+	if ((newRt.left < ptCircle.x && newRt.right > ptCircle.x) &&
+		(newRt.top < ptCircle.y && newRt.bottom > ptCircle.y))
+	{
+		return TRUE;
+	}
+
+	return FALSE;
+}
+BOOL CircleCrashCheck(POINT ptCircle1, int r1, POINT ptCircle2, int r2)
+{
+	float dx = ptCircle1.x - ptCircle2.x;
+	float dy = ptCircle1.y - ptCircle2.y;
+	float l = sqrt(dx * dx + dy * dy);
+
+	if (l <= (r1 + r2)) return TRUE;
+	return FALSE;
+}
