b60de85275e1f95e55ed527d24b2859f3df4831f
ParkHyeonJae <md101106@gmail.com>
2020-06-13 18:48:14 +0900

    UPDATE: Graphics.h Element Previous Writing Script Deleted

--- SkillCompetition/CGame.cpp
+++ SkillCompetition/CGame.cpp
@@ -15,8 +15,9 @@ INT CGame::Init()
 
 	srand((unsigned int)time(0));
 
-	m_sprite = new CSprite(L"../Images/rocket.png", m_Gfx);
-	m_PlayerPos = { (float)(m_dScnX / 2),(float)(m_dScnY / 2) };
+	//m_sprite = new CSprite(L"../Images/rocket.png", m_Gfx);
+
+	/*m_PlayerPos = { (float)(m_dScnX / 2),(float)(m_dScnY / 2) };
 	m_rot = 0.0f;
 	m_PlayerScale = { 1.0f,1.0f };
 	m_playerCenter.x = m_PlayerPos.x + (m_sprite->GetSize().width / 2);
@@ -42,7 +43,7 @@ INT CGame::Init()
 	radius2 = rand() % (m_dScnX - m_dScnY);
 
 	m_bOnceGeometry[0] = TRUE;
-	m_bOnceGeometry[1] = TRUE;
+	m_bOnceGeometry[1] = TRUE;*/
 	return 0;
 }
 
@@ -50,30 +51,31 @@ INT CGame::Render()
 {
 	m_Gfx->BeginDraw();
 	m_Gfx->ClearScreen(0.0f, 0.0f, 0.5f);
-	m_Gfx->GetRenderTarget()->SetTransform(D2D1::Matrix3x2F::Identity());
 
-	
+	//m_Gfx->GetRenderTarget()->SetTransform(D2D1::Matrix3x2F::Identity());
 
-	m_sprite->Draw(m_PlayerPos, &m_playerCenter, D2D1::Point2F(m_PlayerScale.x, m_PlayerScale.y), m_rot);
-	m_Gfx->DrawTextOut(L"20106 박현재", D2D1::Point2F(m_PlayerPos.x - 50, m_PlayerPos.y - 30));
+	//
 
-	m_Gfx->GetRenderTarget()->SetTransform(D2D1::Matrix3x2F::Identity());
-	//m_Gfx->DrawRectangle((float)m_rtPlayerBox.left, (float)m_rtPlayerBox.top, (float)m_rtPlayerBox.right, (float)m_rtPlayerBox.bottom, 1.0f, 1.0f, 1.0f, 1.0f);
-	m_Gfx->DrawRectangle((float)m_rtTargetBox.left, (float)m_rtTargetBox.top, (float)m_rtTargetBox.right, (float)m_rtTargetBox.bottom, 1.0f, 1.0f, 1.0f, 1.0f);
-	m_Gfx->DrawCircle((float)m_TargetCircle.x, (float)m_TargetCircle.y, radius, 1.0f, 1.0f, 1.0f, 1.0f);
+	//m_sprite->Draw(m_PlayerPos, &m_playerCenter, D2D1::Point2F(m_PlayerScale.x, m_PlayerScale.y), m_rot);
+	//m_Gfx->DrawTextOut(L"20106 박현재", D2D1::Point2F(m_PlayerPos.x - 50, m_PlayerPos.y - 30));
 
-	if (m_bOnceGeometry[0])
-	{
-		m_Gfx->DrawRectangle((float)m_rtTargetBox2.left, (float)m_rtTargetBox2.top, (float)m_rtTargetBox2.right, (float)m_rtTargetBox2.bottom, 1.0f, 1.0f, 1.0f, 1.0f);
-		
-	}
-	if (m_bOnceGeometry[1])
-	{
-		m_Gfx->DrawCircle((float)m_TargetCircle2.x, (float)m_TargetCircle2.y, radius2, 1.0f, 1.0f, 1.0f, 1.0f);
+	//m_Gfx->GetRenderTarget()->SetTransform(D2D1::Matrix3x2F::Identity());
+	////m_Gfx->DrawRectangle((float)m_rtPlayerBox.left, (float)m_rtPlayerBox.top, (float)m_rtPlayerBox.right, (float)m_rtPlayerBox.bottom, 1.0f, 1.0f, 1.0f, 1.0f);
+	//m_Gfx->DrawRectangle((float)m_rtTargetBox.left, (float)m_rtTargetBox.top, (float)m_rtTargetBox.right, (float)m_rtTargetBox.bottom, 1.0f, 1.0f, 1.0f, 1.0f);
+	//m_Gfx->DrawCircle((float)m_TargetCircle.x, (float)m_TargetCircle.y, radius, 1.0f, 1.0f, 1.0f, 1.0f);
 
-	}
-	
-	m_Gfx->DrawTextOut(L"이동: 방향키\n 회전: 마우스 방향\n 확대/축소: 마우스 좌/우 클릭", D2D1::Point2F());
+	//if (m_bOnceGeometry[0])
+	//{
+	//	m_Gfx->DrawRectangle((float)m_rtTargetBox2.left, (float)m_rtTargetBox2.top, (float)m_rtTargetBox2.right, (float)m_rtTargetBox2.bottom, 1.0f, 1.0f, 1.0f, 1.0f);
+	//	
+	//}
+	//if (m_bOnceGeometry[1])
+	//{
+	//	m_Gfx->DrawCircle((float)m_TargetCircle2.x, (float)m_TargetCircle2.y, radius2, 1.0f, 1.0f, 1.0f, 1.0f);
+
+	//}
+	//
+	//m_Gfx->DrawTextOut(L"이동: 방향키\n 회전: 마우스 방향\n 확대/축소: 마우스 좌/우 클릭", D2D1::Point2F());
 
 
 	m_Gfx->EndDraw();
@@ -84,7 +86,7 @@ INT CGame::FrameMove(DWORD elpased)
 {
 	CDX2DAPP::FrameMove(elpased);
 
-	m_playerCenter.x = m_PlayerPos.x + (m_sprite->GetSize().width / 2);
+	/*m_playerCenter.x = m_PlayerPos.x + (m_sprite->GetSize().width / 2);
 	m_playerCenter.y = m_PlayerPos.y + (m_sprite->GetSize().height / 2);
 	m_rtPlayerBox = { (LONG)m_playerCenter.x - 120,
 		(LONG)m_playerCenter.y - 120,
@@ -116,7 +118,7 @@ INT CGame::FrameMove(DWORD elpased)
 	}
 	if (IsRectInCircle(m_TargetCircle2, radius2, m_rtPlayerBox)) {
 		m_bOnceGeometry[1] = FALSE;
-	}
+	}*/
 	return 0;
 }
 
@@ -128,33 +130,33 @@ INT CGame::Control(CInput* Input)
 {
 	POINT MousePos = Input->GetMousePos();
 
-	float rotdegree = atan2f(m_PlayerPos.y - MousePos.y, m_PlayerPos.x - MousePos.x);
-	
-	m_rot = (rotdegree * (180 / PI)) - 90;
+	//float rotdegree = atan2f(m_PlayerPos.y - MousePos.y, m_PlayerPos.x - MousePos.x);
+	//
+	//m_rot = (rotdegree * (180 / PI)) - 90;
 
-	
-	//printf("%f\n", m_rot);
+	//
+	////printf("%f\n", m_rot);
 
-	if (Input->KeyPress(VK_LEFT))
-		m_PlayerPos.x -= 10;
-	if (Input->KeyPress(VK_RIGHT))
-		m_PlayerPos.x += 10;
-	if (Input->KeyPress(VK_UP))
-		m_PlayerPos.y -= 10;
-	if (Input->KeyPress(VK_DOWN))
-		m_PlayerPos.y += 10;
+	//if (Input->KeyPress(VK_LEFT))
+	//	m_PlayerPos.x -= 10;
+	//if (Input->KeyPress(VK_RIGHT))
+	//	m_PlayerPos.x += 10;
+	//if (Input->KeyPress(VK_UP))
+	//	m_PlayerPos.y -= 10;
+	//if (Input->KeyPress(VK_DOWN))
+	//	m_PlayerPos.y += 10;
 
 
-	if (Input->BtnDown(0) || Input->BtnPress(0))
-	{
-		m_PlayerScale.x += 0.1f;
-		m_PlayerScale.y += 0.1f;
-	}
-	if (Input->BtnDown(1) || Input->BtnPress(1))
-	{
-		m_PlayerScale.x -= 0.1f;
-		m_PlayerScale.y -= 0.1f;
-	}
+	//if (Input->BtnDown(0) || Input->BtnPress(0))
+	//{
+	//	m_PlayerScale.x += 0.1f;
+	//	m_PlayerScale.y += 0.1f;
+	//}
+	//if (Input->BtnDown(1) || Input->BtnPress(1))
+	//{
+	//	m_PlayerScale.x -= 0.1f;
+	//	m_PlayerScale.y -= 0.1f;
+	//}
 
 
 
--- SkillCompetition/CGame.h
+++ SkillCompetition/CGame.h
@@ -1,26 +1,26 @@
 #pragma once
 class CGame : public CDX2DAPP
 {
-	CSprite* m_sprite;
-	D2D1_POINT_2F m_playerCenter;
-	D2D1_POINT_2F m_PlayerPos;
-	D2D1_POINT_2F m_PlayerScale;
-	RECT m_rtPlayerBox;
+	//CSprite* m_sprite;
+	//D2D1_POINT_2F m_playerCenter;
+	//D2D1_POINT_2F m_PlayerPos;
+	//D2D1_POINT_2F m_PlayerScale;
+	//RECT m_rtPlayerBox;
 
-	RECT m_rtTargetBox;
-	RECT m_rtTargetBox2;
-	
+	//RECT m_rtTargetBox;
+	//RECT m_rtTargetBox2;
+	//
 
-	POINT m_TargetCircle;
-	int radius;
+	//POINT m_TargetCircle;
+	//int radius;
 
-	POINT m_TargetCircle2;
-	int radius2;
+	//POINT m_TargetCircle2;
+	//int radius2;
 
-	float m_rot;
-	BOOL m_bIsCrash;
-	BOOL m_bIsCircleCrash;
-	BOOL m_bOnceGeometry[2];
+	//float m_rot;
+	//BOOL m_bIsCrash;
+	//BOOL m_bIsCircleCrash;
+	//BOOL m_bOnceGeometry[2];
 public:
 	CGame();
 	~CGame();
